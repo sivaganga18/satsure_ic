9b95d88b99b0e2014a991f6cfd6592c97f0d127d
sivadotball <siva@dotball.com>
2019-08-09 13:24:59 +0530

    dropdown added

--- src/components/Common/AppHeader.js
+++ src/components/Common/AppHeader.js
@@ -2,10 +2,23 @@ import React, { Component } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import { colors, typography } from "../../styles/StyleSheet";
 import CustomStatusBar from "./CustomStatusBar";
+import Menu, { MenuItem, MenuDivider } from "react-native-material-menu";
+import { map } from "lodash";
 
 export default class AppHeader extends Component {
   render() {
-    const { leftIcon, leftIconCallback, title } = this.props;
+    const {
+      leftIcon,
+      leftIconCallback,
+      title,
+      menuCallback,
+      menuListCallback,
+      menuTitle,
+      menuRef,
+      menuList,
+      rightIcon,
+      showMenu
+    } = this.props;
     return (
       <View>
         <CustomStatusBar
@@ -16,12 +29,13 @@ export default class AppHeader extends Component {
           style={{
             flexDirection: "row",
             alignItems: "center",
+            justifyContent: "space-between",
             height: 60,
             backgroundColor: colors.drakGreen
           }}
         >
           <View
-            style={{ flex: 0.5, flexDirection: "row", alignItems: "center" }}
+            style={{ flex: 0.6, flexDirection: "row", alignItems: "center" }}
           >
             <TouchableOpacity
               activeOpacity={1}
@@ -39,6 +53,42 @@ export default class AppHeader extends Component {
                 {title}
               </Text>
             </View>
+          </View>
+          <View style={{ flex: 0.4, alignItems: "flex-end" }}>
+            {showMenu ? (
+              <Menu
+                ref={menuRef ? menuRef : "ref"}
+                button={
+                  <View style={{ paddingRight: 16, flexDirection: "row" }}>
+                    <Text
+                      style={[
+                        typography.medium.medium,
+                        { color: colors.white }
+                      ]}
+                      onPress={menuCallback}
+                    >
+                      {menuTitle}
+                    </Text>
+                  </View>
+                }
+              >
+                {map(menuList, (menu, key) => {
+                  return (
+                    <MenuItem
+                      onPress={() => {
+                        menuListCallback(menu.value);
+                      }}
+                    >
+                      {menu.value}
+                    </MenuItem>
+                  );
+                })}
+              </Menu>
+            ) : rightIcon ? (
+              <View />
+            ) : (
+              <View />
+            )}
           </View>
         </View>
       </View>
--- src/components/Scenes/Portfolio.js
+++ src/components/Scenes/Portfolio.js
@@ -4,7 +4,22 @@ import AppHeader from "../Common/AppHeader";
 import PortfolioLayout from "../ScenesComponents/Portfolio/PortfolioLayout";
 
 export default class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuTitle: "WhiteField"
+    };
+  }
+  hideMenu = value => {
+    this.setState({ menuTitle: value });
+    this._menu.hide();
+  };
+
+  showMenu = () => {
+    this._menu.show();
+  };
   render() {
+    const { menuTitle } = this.state;
     return (
       <View style={{ flex: 1 }}>
         {/* Header */}
@@ -12,6 +27,23 @@ export default class Portfolio extends Component {
           leftIcon={require("../../assets/images/hamburger.png")}
           leftIconCallback={() => {}}
           title="Priority Portfolio"
+          menuCallback={() => {
+            this.showMenu();
+          }}
+          menuListCallback={value => {
+            this.hideMenu(value);
+          }}
+          menuTitle={menuTitle}
+          menuRef={ref => {
+            this._menu = ref;
+          }}
+          menuList={[
+            { value: "Whitefield" },
+            { value: "Kasthuri Nagar" },
+            { value: "Hebbal" },
+            { value: "Hosur" }
+          ]}
+          showMenu={true}
         />
         {/* Header */}
 
